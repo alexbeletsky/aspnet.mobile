4dc24cef714165db8a062cdff69302862a64faa3
alexander.beletsky <alexander.beletsky@gmail.com>
2012-03-13 22:46:44 +0200

    controls added

--- AspNet.Portal.Mobile/Scripts/App/ViewModels/ArticlesViewModel.js
+++ AspNet.Portal.Mobile/Scripts/App/ViewModels/ArticlesViewModel.js
@@ -4,12 +4,35 @@
 
         var self = this;
         self.articles = ko.observableArray([]);
+        self.currentPage = ko.observable(1);
 
         $.get('/api/articles', function (r) {
-            var mapped = $.map(r, function (a) { return new Article(a); });
-            self.articles(mapped);
+            updateArticles(r);
         });
 
+        self.prevPageClicked = function () {
+            var prevPage = self.currentPage() - 1;
+            if (prevPage == 0)
+                return false;
+
+            $.get('/api/articles/' + prevPage, function (r) {
+                self.currentPage(prevPage);
+                updateArticles(r);
+            });
+        }
+
+        self.nextPageClicked = function () {
+            var nextPage = self.currentPage() + 1;
+            $.get('/api/articles/' + nextPage, function (r) {
+                self.currentPage(nextPage);
+                updateArticles(r);
+            });
+        }
+
+        var updateArticles = function (r) {
+            var mapped = $.map(r, function (a) { return new Article(a); });
+            self.articles(mapped);
+        }
     };
 
     ko.applyBindings(new ArticlesViewModel());
