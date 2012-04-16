49d135909e834ca06ff18e16034a4c36dea8d854
alexander.beletsky <alexander.beletsky@gmail.com>
2012-04-16 11:56:56 +0300

    implemented 'More...' button

--- AspNet.Portal.Mobile/Scripts/App/ViewModels/ArticlesViewModel.js
+++ AspNet.Portal.Mobile/Scripts/App/ViewModels/ArticlesViewModel.js
@@ -6,33 +6,36 @@ function ArticlesViewModel() {
     self.articles = ko.observableArray([]);
     self.currentPage = ko.observable(1);
 
-    self.prevPageClicked = function () {
-        var prevPage = self.currentPage() - 1;
-        if (prevPage == 0)
-            return false;
-
-        self.currentPage(prevPage);
-
-        gotoPage(prevPage);
-                    
-        return true;
-    };
-
-    self.nextPageClicked = function () {
-        var nextPage = self.currentPage() + 1;
-        self.currentPage(nextPage);
-
-        gotoPage(nextPage);
-
-        return true;
-    };
+    // public
 
     self.onArticleClicked = function (article) {
         article.setRead();
     };
 
+    self.refresh = function () {
+        setTimeout(loadArticles, 500);
+    }
+
+    self.showMore = function () {
+        var nextPage = self.currentPage() + 1;
+        self.currentPage(nextPage);
+
+        $.mobile.showPageLoadingMsg();
+        $.get('/api/articles/' + nextPage, function (r) {
+            updateArticles(r);
+
+            $.mobile.hidePageLoadingMsg();
+        });
+    }
+
+    // private
+
     var loadArticles = function () {
         $.mobile.showPageLoadingMsg();
+
+        // init
+        self.articles.removeAll();
+        self.currentPage(1);
 
         $('.content').hide();
         $.get('/api/articles', function (r) {
@@ -45,7 +48,7 @@ function ArticlesViewModel() {
 
     var updateArticles = function (r) {
         var mapped = $.map(r, function (a) { return new Article().fromData(a); });
-        self.articles(mapped);
+        _.each(mapped, function (article) { self.articles.push(article); });
     };
     
     var gotoPage = function (nextPage) {
@@ -58,10 +61,5 @@ function ArticlesViewModel() {
         });
     }
 
-    $('#refresh').on('click', function () {
-        setTimeout(loadArticles, 500);
-    });
-
     loadArticles();
-
 };
